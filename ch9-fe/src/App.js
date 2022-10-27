1b6deeb7d29aa7bb709a27108f2202586260d6f6
rkrobby19 <rizkyrobby19@gmail.com>
2022-10-27 17:57:30 +0700

    Add - navbar

--- ch9-fe/src/App.js
+++ ch9-fe/src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/landing'
-import Login from './pages/login_register';
-import Game from './pages/game_detail';
-import Profile from './pages/profile';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/landing";
+import Login from "./pages/login_register";
+import Game from "./pages/game_detail";
+import Profile from "./pages/profile";
+import Layout from "./components/Layout";
 
 function App() {
-  return (
-   <BrowserRouter>
-    <Routes>
-      <Route path="/landing" element={<Landing/>}/>
-      <Route path="/login_register" element={<Login/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-      <Route path="/game" element={<Game/>}/>
-    </Routes>
-   </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Landing />} />
+                    <Route path="profile" element={<Profile />} />
+                    <Route path="game" element={<Game />} />
+                    <Route path="login_register" element={<Login />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
