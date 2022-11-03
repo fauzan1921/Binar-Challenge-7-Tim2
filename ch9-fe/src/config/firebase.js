016b3d6940b6ab7a163c8cb2f514c72698e07260
IrwanFicoFar <irwanhadi9@gmail.com>
2022-11-03 07:23:46 +0700

    authentication register & add firebase.example

--- ch9-fe/src/config/firebase.js
+++ ch9-fe/src/config/firebase.js
@@ -1,13 +1,17 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: EACT_APP_FIREBASE_API_KEY,
-    authDomain: "authentication-3bf42.firebaseapp.com",
-    projectId: "authentication-3bf42",
-    storageBucket: "authentication-3bf42.appspot.com",
-    messagingSenderId: "345446161095",
-    appId: "1:345446161095:web:232d46abb98c4592e7fce2",
-    measurementId: "G-D2ZQZCMCFS"
+    apiKey: "AIzaSyAhrcXHsh2XoZ1VuIhepvBOofYTHiUNci4",
+  authDomain: "authentication-3bf42.firebaseapp.com",
+  projectId: "authentication-3bf42",
+  storageBucket: "authentication-3bf42.appspot.com",
+  messagingSenderId: "345446161095",
+  appId: "1:345446161095:web:232d46abb98c4592e7fce2",
+  measurementId: "G-D2ZQZCMCFS"
 };
 
 const app = initializeApp(firebaseConfig);
+const authFirebase = getAuth(app);
+
+export default authFirebase
