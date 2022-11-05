cf4a539650f1bf2cdf70b1cd22f8cf90bbf00891
IrwanFicoFar <irwanhadi9@gmail.com>
2022-11-05 09:13:42 +0700

    adding localstorage jwt

--- ch9-fe/src/pages/game_detail.jsx
+++ ch9-fe/src/pages/game_detail.jsx
@@ -1,133 +1,151 @@
-import { Component } from 'react';
-import { Col, Row, Table } from 'react-bootstrap';
-import GameLeaderboard from '../components/GameLeaderboard';
-import ScissorGame from '../components/ScissorGame';
-import '../css/game_detail.css';
-
+import { Component } from "react";
+import { Col, Row, Table } from "react-bootstrap";
+import GameLeaderboard from "../components/GameLeaderboard";
+import ScissorGame from "../components/ScissorGame";
+import "../css/game_detail.css";
 
 class Game extends Component {
+  state = {
+    playerBackgroundColor: {
+      kertas: false,
+      batu: false,
+      gunting: false,
+    },
+    comBackgroundColor: {
+      kertas: false,
+      batu: false,
+      gunting: false,
+    },
+    isCLick: false,
+    playerChoice: "",
+    comChoice: "",
+    result: "VS",
+  };
 
-    state = {
-        playerBackgroundColor : {
-            kertas : false,
-            batu : false,
-            gunting : false
-        },
-        comBackgroundColor : {
-            kertas : false,
-            batu : false,
-            gunting : false
-        },
-        isCLick : false,
-        playerChoice : '',
-        comChoice : '',
-        result : 'VS'
+  handlingMouseEnter = (event) => {
+    this.setState({
+      playerBackgroundColor: { [event.target.id]: true },
+    });
+  };
 
+  handlingMouseLeave = (event) => {
+    this.setState({
+      playerBackgroundColor: { [event.target.id]: false },
+    });
+  };
+
+  handlingOnClick = (event) => {
+    this.setState({
+      isCLick: true,
+      playerChoice: event.target.id,
+    });
+    this.generateRandom();
+  };
+
+  generateRandom = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+      this.setState({
+        comBackgroundColor: { gunting: true },
+        comChoice: "gunting",
+      });
+    } else if (randomNumber === 1) {
+      this.setState({
+        comBackgroundColor: { kertas: true },
+        comChoice: "kertas",
+      });
+    } else {
+      this.setState({
+        comBackgroundColor: { batu: true },
+        comChoice: "batu",
+      });
+    }
+  };
+
+  compareChoice = () => {
+    console.log(this.state.playerChoice);
+    console.log(this.state.comChoice);
+
+    if (this.state.comChoice === this.state.playerChoice) {
+      this.setState({
+        result: "DRAW",
+      });
+    } else if (
+      this.state.comChoice === "gunting" &&
+      this.state.playerChoice === "batu"
+    ) {
+      this.setState({
+        result: "PLAYER WIN",
+      });
+    } else if (
+      this.state.comChoice === "gunting" &&
+      this.state.playerChoice === "kertas"
+    ) {
+      this.setState({
+        result: "COM WIN",
+      });
+    } else if (
+      this.state.comChoice === "batu" &&
+      this.state.playerChoice === "kertas"
+    ) {
+      this.setState({
+        result: "PLAYER WIN",
+      });
+    } else if (
+      this.state.comChoice === "batu" &&
+      this.state.playerChoice === "gunting"
+    ) {
+      this.setState({
+        result: "COM WIN",
+      });
+    } else if (
+      this.state.comChoice === "kertas" &&
+      this.state.playerChoice === "gunting"
+    ) {
+      this.setState({
+        result: "PLAYER WIN",
+      });
+    } else if (
+      this.state.comChoice === "kertas" &&
+      this.state.playerChoice === "batu"
+    ) {
+      this.setState({
+        result: "COM WIN",
+      });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.comChoice != this.state.comChoice) {
+      this.compareChoice();
+    }
+  }
+
+  render() {
+    console.log(localStorage.getItem("jwt-token"));
+
+    if (localStorage.getItem("jwt-token") === null) {
+      window.location.href = "/login";
     }
 
-
-    handlingMouseEnter = (event) => {
-        this.setState({
-            playerBackgroundColor : { [event.target.id] : true }
-        })
-    }
-
-    handlingMouseLeave = (event) => {
-        this.setState({
-            playerBackgroundColor : { [event.target.id] : false }
-        })
-    }
-
-    handlingOnClick = (event) => {
-        this.setState({
-            isCLick : true,
-            playerChoice : event.target.id,
-        })
-        this.generateRandom()
-    }
-
-    generateRandom = () => {
-        const randomNumber = Math.floor(Math.random()*3) 
-
-        if(randomNumber === 0) {
-            this.setState({
-                comBackgroundColor : {gunting : true},
-                comChoice : 'gunting'
-            })
-        } else if (randomNumber === 1) {
-            this.setState({
-                comBackgroundColor : {kertas : true},
-                comChoice : 'kertas'
-            })
-        } else {
-            this.setState({
-                comBackgroundColor : {batu : true},
-                comChoice : 'batu'
-            })
-        }
-    }
-
-    compareChoice = () => {
-        console.log(this.state.playerChoice)
-        console.log(this.state.comChoice)
-
-        if (this.state.comChoice === this.state.playerChoice){
-            this.setState({
-                result : 'DRAW'
-            })
-        } else if (this.state.comChoice === 'gunting' && this.state.playerChoice === 'batu'){
-            this.setState({
-                result : 'PLAYER WIN'
-            })
-        } else if (this.state.comChoice === 'gunting' && this.state.playerChoice === 'kertas'){
-            this.setState({
-                result : 'COM WIN'
-            })
-        } else if (this.state.comChoice === 'batu' && this.state.playerChoice === 'kertas'){
-            this.setState({
-                result : 'PLAYER WIN'
-            })
-        } else if (this.state.comChoice === 'batu' && this.state.playerChoice === 'gunting'){
-            this.setState({
-                result : 'COM WIN'
-            })
-        } else if (this.state.comChoice === 'kertas' && this.state.playerChoice === 'gunting'){
-            this.setState({
-                result : 'PLAYER WIN'
-            })
-        } else if(this.state.comChoice === 'kertas' && this.state.playerChoice === 'batu'){
-            this.setState({
-                result : 'COM WIN'
-            })
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState){
-        if(prevState.comChoice != this.state.comChoice){
-            this.compareChoice()
-        }
-    }
-
-
-    render(){
-        return(
-            <div className='game-detail'>
-                <Row>
-                    <ScissorGame 
-                    handlingMouseEnter={this.handlingMouseEnter} 
-                    handlingMouseLeave={this.handlingMouseLeave}
-                    playerBackgroundColor={this.state.playerBackgroundColor}
-                    comBackgroundColor={this.state.comBackgroundColor}
-                    handlingOnClick={this.handlingOnClick}
-                    isCLick={this.state.isCLick}
-                    result={this.state.result}
-                    />
-                    <GameLeaderboard />
-                </Row>
-                
-            </div>
-        )
-    }
+    return (
+      <div className="game-detail">
+        <Row>
+          <ScissorGame
+            handlingMouseEnter={this.handlingMouseEnter}
+            handlingMouseLeave={this.handlingMouseLeave}
+            playerBackgroundColor={this.state.playerBackgroundColor}
+            comBackgroundColor={this.state.comBackgroundColor}
+            handlingOnClick={this.handlingOnClick}
+            isCLick={this.state.isCLick}
+            result={this.state.result}
+          />
+          <GameLeaderboard />
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Game;
