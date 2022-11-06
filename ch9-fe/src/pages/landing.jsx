c93c62d286057543d2c2f704a5840aae32e01ca4
Fauzan Aslam Kusumah <107337182+fauzan1921@users.noreply.github.com>
2022-11-06 10:15:43 +0700

    Update landing.jsx
    nambah fungsi auth di button untuk ke game-detail

--- ch9-fe/src/pages/landing.jsx
+++ ch9-fe/src/pages/landing.jsx
@@ -1,16 +1,27 @@
-const Landing = () => {
-  return (
-    <div>
-      <div className="container mt-5">
-        <h1 className="bold">Game List</h1>
-        <ul>
-          <li>
-            <button>Rock, Paper, Scissors</button>
-          </li>
-        </ul>
+import { Component } from "react";
+
+class Landing extends Component{
+
+  goToGameDetail = () => {
+    if (localStorage.getItem("jwt-token") !== null) {
+      window.location.href = "/game";
+    }else{
+      alert('You have to login first')
+    }}
+  render(){    
+    return (
+      <div>
+        <div className="container mt-5">
+          <h1 className="bold">Game List</h1>
+          <ul>
+            <li>
+              <button className="btn btn-warning" onClick={this.goToGameDetail}>Rock, Paper, Scissors</button>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Landing;
