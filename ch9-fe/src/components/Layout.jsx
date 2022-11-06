48d6c148ff5d1bb687d1164fb6ed15955ac1401b
rkrobby19 <rizkyrobby19@gmail.com>
2022-11-06 22:04:16 +0700

    Add logout button on navbar

--- ch9-fe/src/components/Layout.jsx
+++ ch9-fe/src/components/Layout.jsx
@@ -1,32 +1,80 @@
+import React, { useState, useEffect } from "react";
+
 import { Outlet } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { getBiodataById } from "../action/biodata";
 
-const Layout = () => {
-  return (
-    <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">Kelompok 2</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/profile">Profile</Nav.Link>
-              <Nav.Link href="/game">Game</Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="/login">Login</Nav.Link>
-              <Nav.Link href="/register">Register</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+const Layout = (props) => {
+    const { user } = props;
 
-      <Outlet />
-    </>
-  );
+    const [biodata, setBiodata] = useState({
+        email: "Loading...",
+        fullname: "Loading...",
+        username: "Loading...",
+    });
+
+    const getUserBiodata = async () => {
+        const id = user.user_id;
+        const resp = await getBiodataById(id);
+        setBiodata(resp);
+    };
+
+    const handleLogout = () => {
+        let ans = window.confirm(`Are you sure want to logout?`);
+        if (ans) {
+            localStorage.removeItem("jwt-token");
+            alert(`Thanks for coming`);
+        }
+    };
+
+    useEffect(() => {
+        getUserBiodata();
+        console.log(user);
+    }, []);
+
+    return (
+        <>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">Kelompok 2</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/profile">Profile</Nav.Link>
+                            <Nav.Link href="/game">Game</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {!user ? (
+                                <>
+                                    <Nav.Link href="/login">Login</Nav.Link>
+                                    <Nav.Link href="/register">
+                                        Register
+                                    </Nav.Link>
+                                </>
+                            ) : (
+                                <>
+                                    <Nav.Link href="/profile">
+                                        Hello, {biodata.username}
+                                    </Nav.Link>
+                                    <Nav.Link
+                                        href="/login"
+                                        onClick={handleLogout}
+                                    >
+                                        Logout
+                                    </Nav.Link>
+                                </>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Outlet />
+        </>
+    );
 };
 
 export default Layout;
--- ch9-fe/src/pages/game_detail.jsx
+++ ch9-fe/src/pages/game_detail.jsx
@@ -5,159 +5,163 @@ import ScissorGame from "../components/ScissorGame";
 import "../css/game_detail.css";
 
 class Game extends Component {
-  state = {
-    playerBackgroundColor: {
-      kertas: false,
-      batu: false,
-      gunting: false,
-    },
-    comBackgroundColor: {
-      kertas: false,
-      batu: false,
-      gunting: false,
-    },
-    isCLick: false,
-    playerChoice: "",
-    comChoice: "",
-    result: "VS",
-  };
+    state = {
+        playerBackgroundColor: {
+            kertas: false,
+            batu: false,
+            gunting: false,
+        },
+        comBackgroundColor: {
+            kertas: false,
+            batu: false,
+            gunting: false,
+        },
+        isCLick: false,
+        playerChoice: "",
+        comChoice: "",
+        result: "VS",
+    };
 
-  handleLogout = () => {
-    localStorage.removeItem("jwt-token");
-    alert("YOU ARE LOGOUT");
-    window.location.href = "/";
-  };
+    handleLogout = () => {
+        localStorage.removeItem("jwt-token");
+        alert("YOU ARE LOGOUT");
+        window.location.href = "/";
+    };
 
-  handlingMouseEnter = (event) => {
-    this.setState({
-      playerBackgroundColor: { [event.target.id]: true },
-    });
-  };
+    handlingMouseEnter = (event) => {
+        this.setState({
+            playerBackgroundColor: { [event.target.id]: true },
+        });
+    };
 
-  handlingMouseLeave = (event) => {
-    this.setState({
-      playerBackgroundColor: { [event.target.id]: false },
-    });
-  };
+    handlingMouseLeave = (event) => {
+        this.setState({
+            playerBackgroundColor: { [event.target.id]: false },
+        });
+    };
 
-  handlingOnClick = (event) => {
-    this.setState({
-      isCLick: true,
-      playerChoice: event.target.id,
-    });
-    this.generateRandom();
-  };
+    handlingOnClick = (event) => {
+        this.setState({
+            isCLick: true,
+            playerChoice: event.target.id,
+        });
+        this.generateRandom();
+    };
 
-  generateRandom = () => {
-    const randomNumber = Math.floor(Math.random() * 3);
+    generateRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 3);
 
-    if (randomNumber === 0) {
-      this.setState({
-        comBackgroundColor: { gunting: true },
-        comChoice: "gunting",
-      });
-    } else if (randomNumber === 1) {
-      this.setState({
-        comBackgroundColor: { kertas: true },
-        comChoice: "kertas",
-      });
-    } else {
-      this.setState({
-        comBackgroundColor: { batu: true },
-        comChoice: "batu",
-      });
-    }
-  };
+        if (randomNumber === 0) {
+            this.setState({
+                comBackgroundColor: { gunting: true },
+                comChoice: "gunting",
+            });
+        } else if (randomNumber === 1) {
+            this.setState({
+                comBackgroundColor: { kertas: true },
+                comChoice: "kertas",
+            });
+        } else {
+            this.setState({
+                comBackgroundColor: { batu: true },
+                comChoice: "batu",
+            });
+        }
+    };
 
-  compareChoice = () => {
-    console.log(this.state.playerChoice);
-    console.log(this.state.comChoice);
+    compareChoice = () => {
+        console.log(this.state.playerChoice);
+        console.log(this.state.comChoice);
 
-    if (this.state.comChoice === this.state.playerChoice) {
-      this.setState({
-        result: "DRAW",
-      });
-    } else if (
-      this.state.comChoice === "gunting" &&
-      this.state.playerChoice === "batu"
-    ) {
-      this.setState({
-        result: "PLAYER WIN",
-      });
-    } else if (
-      this.state.comChoice === "gunting" &&
-      this.state.playerChoice === "kertas"
-    ) {
-      this.setState({
-        result: "COM WIN",
-      });
-    } else if (
-      this.state.comChoice === "batu" &&
-      this.state.playerChoice === "kertas"
-    ) {
-      this.setState({
-        result: "PLAYER WIN",
-      });
-    } else if (
-      this.state.comChoice === "batu" &&
-      this.state.playerChoice === "gunting"
-    ) {
-      this.setState({
-        result: "COM WIN",
-      });
-    } else if (
-      this.state.comChoice === "kertas" &&
-      this.state.playerChoice === "gunting"
-    ) {
-      this.setState({
-        result: "PLAYER WIN",
-      });
-    } else if (
-      this.state.comChoice === "kertas" &&
-      this.state.playerChoice === "batu"
-    ) {
-      this.setState({
-        result: "COM WIN",
-      });
-    }
-  };
+        if (this.state.comChoice === this.state.playerChoice) {
+            this.setState({
+                result: "DRAW",
+            });
+        } else if (
+            this.state.comChoice === "gunting" &&
+            this.state.playerChoice === "batu"
+        ) {
+            this.setState({
+                result: "PLAYER WIN",
+            });
+        } else if (
+            this.state.comChoice === "gunting" &&
+            this.state.playerChoice === "kertas"
+        ) {
+            this.setState({
+                result: "COM WIN",
+            });
+        } else if (
+            this.state.comChoice === "batu" &&
+            this.state.playerChoice === "kertas"
+        ) {
+            this.setState({
+                result: "PLAYER WIN",
+            });
+        } else if (
+            this.state.comChoice === "batu" &&
+            this.state.playerChoice === "gunting"
+        ) {
+            this.setState({
+                result: "COM WIN",
+            });
+        } else if (
+            this.state.comChoice === "kertas" &&
+            this.state.playerChoice === "gunting"
+        ) {
+            this.setState({
+                result: "PLAYER WIN",
+            });
+        } else if (
+            this.state.comChoice === "kertas" &&
+            this.state.playerChoice === "batu"
+        ) {
+            this.setState({
+                result: "COM WIN",
+            });
+        }
+    };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.comChoice != this.state.comChoice) {
-      this.compareChoice();
-    }
-  }
-
-  render() {
-    console.log(localStorage.getItem("jwt-token"));
-
-    if (localStorage.getItem("jwt-token") === null) {
-      window.location.href = "/login";
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.comChoice != this.state.comChoice) {
+            this.compareChoice();
+        }
     }
 
-    return (
-      <div className="game-detail">
-        <Row>
-          <ScissorGame
-            handlingMouseEnter={this.handlingMouseEnter}
-            handlingMouseLeave={this.handlingMouseLeave}
-            playerBackgroundColor={this.state.playerBackgroundColor}
-            comBackgroundColor={this.state.comBackgroundColor}
-            handlingOnClick={this.handlingOnClick}
-            isCLick={this.state.isCLick}
-            result={this.state.result}
-          />
-          <GameLeaderboard />
-          <div className=" text-center gap-2 mt-4">
-            <Button variant="danger" type="submit" onClick={this.handleLogout}>
-              LOGOUT
-            </Button>
-            ;
-          </div>
-        </Row>
-      </div>
-    );
-  }
+    render() {
+        console.log(localStorage.getItem("jwt-token"));
+
+        if (localStorage.getItem("jwt-token") === null) {
+            window.location.href = "/login";
+        }
+
+        return (
+            <div className="game-detail">
+                <Row>
+                    <ScissorGame
+                        handlingMouseEnter={this.handlingMouseEnter}
+                        handlingMouseLeave={this.handlingMouseLeave}
+                        playerBackgroundColor={this.state.playerBackgroundColor}
+                        comBackgroundColor={this.state.comBackgroundColor}
+                        handlingOnClick={this.handlingOnClick}
+                        isCLick={this.state.isCLick}
+                        result={this.state.result}
+                    />
+                    <GameLeaderboard />
+                    <div className=" text-center gap-2 mt-4">
+                        <Button
+                            variant="danger"
+                            type="submit"
+                            onClick={this.handleLogout}
+                        >
+                            LOGOUT
+                        </Button>
+                        ;
+                    </div>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default Game;
