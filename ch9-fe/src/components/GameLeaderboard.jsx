2ed3fa4e08d9be3c007089e99b67687aa3488537
rkrobby19 <rizkyrobby19@gmail.com>
2022-11-09 08:24:12 +0700

    Add link to navigate see other player profile

--- ch9-fe/src/components/GameLeaderboard.jsx
+++ ch9-fe/src/components/GameLeaderboard.jsx
@@ -1,33 +1,35 @@
 import { Component } from "react";
-import { Col, Row, Table } from 'react-bootstrap';
-
+import { Col, Row, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class GameLeaderboard extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <Col sm={4} className="game-leaderboard">
                 <h1 class="leaderboard-text">GAME LEADERBOARD</h1>
                 <Table striped bordered hover>
                     <thead>
                         <tr>
-                        <th>Username</th>
-                        <th>Win</th>
+                            <th>Username</th>
+                            <th>Win</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                        this.props.leaderboard.map(e => (
+                        {this.props.leaderboard.map((e) => (
                             <tr>
-                            <td>{e.data.username}</td>
-                            <td>{e.data.win}</td>
+                                <td>
+                                    <Link to={`/player/${e.id}`}>
+                                        {e.data.username}
+                                    </Link>
+                                </td>
+                                <td>{e.data.win}</td>
                             </tr>
-                        ))
-                        }
+                        ))}
                     </tbody>
                 </Table>
             </Col>
-        )
+        );
     }
 }
 
-export default GameLeaderboard
+export default GameLeaderboard;
